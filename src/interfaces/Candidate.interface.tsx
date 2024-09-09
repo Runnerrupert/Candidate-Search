6af5f7cb4ab1b5e7bd0c121099b0b9e5646ad9a2
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-09 09:57:50 -0600

    Create interface for each candidate/user, create the nav bar for both pages

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,8 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    "login": string;
+    "company": string;
+    "location": string;
+    "email": string;
+    "bio": string;
+}
