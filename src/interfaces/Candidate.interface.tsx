73ba4252dae5255207513ab56aa531061eb646d8
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-09-09 12:06:36 -0600

    Add styling to home page, create a base template for the candidateCards

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,5 +1,6 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 interface Candidate {
+    "avatar_url": string;
     "login": string;
     "company": string;
     "location": string;
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,28 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
+
+
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  return (
+    <>
+    <h1>CandidateSearch</h1>
+    <div className="candidateCard">
+      <img></img>
+      <div className="candidate">
+        <h2>UserName(Username)</h2>
+        <p>Location:</p>
+        <p>Email:</p>
+        <p>Company:</p>
+        <p>Bio:</p>
+      </div>
+    </div>
+    <div className="btns">
+      <button className="minus-btn">-</button>
+      <button className="add-btn">+</button>
+    </div>
+    </>
+  );
 };
 
 export default CandidateSearch;
